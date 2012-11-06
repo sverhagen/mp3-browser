e4bc913758b385ebcb39617b8dc3af6be49b2aae
Sander Verhagen <verhagen@sander>
2012-11-05 23:02:57 -0800

    Prepare folder structure for package extension with mp3browser and mp3smartsearch plugins both

---
+++ src/main/php/mp3smartsearch/mp3smartsearch.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of mp3 Browser.
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License (V2) along with this. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Previous copyright likely held by others such as Jon Hollis, Luke Collymore, as associated with
+ * dotcomdevelopment.com.
+ * Copyright 2012 Sander Verhagen (verhagen@sander.com).
+ */
